d6f6881c44a27658a3a7cac6b2a2b769f385ff29
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-10-27 10:14:19 -0400

    App window's size has been set to a min and max width and height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,37 @@
 <template>
-	<div class="home bg-gray-400 h-screen p-2">
-		<div class="grid grid-cols-2 bg-white rounded-lg w-3/4 h-3/4 m-auto overflow-hidden">
-			<div>
-				<div class="p-5">
-					<img class="h-auto w-4/6" src="@/assets/logo.svg" />
-				</div>
-				<div class="mt-20 mr-10">
-					<img src="@/assets/artifacts.svg" />
-				</div>
-			</div>
-			<div class="mt-20">
-				<div class="space-y-1 mb-4">
-					<h1 class="font-bold text-lg">{{headerText}}</h1>
-					<p class="text-sm">Or <span class="underline cursor-pointer" v-on:click="toggleView">{{subheadingText}} &#8594;</span></p>
-				</div>
-				<div class="space-y-5">
-					<div class="mr-14">
-						<label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-						<div class="mt-1">
-							<input v-model="email" type="email" name="email" id="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com" />
-						</div>
-					</div>
-					<div class="mr-14">
-						<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-						<div class="mt-1">
-							<input v-model="password" type="password" name="password" id="password" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="••••" />
-						</div>
-					</div>
-					<button v-on:click="buttonClick" type="button" class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-storjBlue focus:outline-none focus:ring-2 focus:ring-offset-2">
-						{{buttonText}}
-					</button>
-				</div>
-			</div>
+<div class="grid grid-cols-2 bg-white m-auto overflow-hidden">
+	<div>
+		<div class="p-5">
+			<img class="h-auto w-3/5" src="@/assets/logo.svg" />
+		</div>
+		<div class="mt-20 mr-10">
+			<img class="w-auto h-auto" src="@/assets/artifacts.svg" />
 		</div>
 	</div>
+	<div class="mt-20">
+		<div class="space-y-1 mb-4">
+			<h1 class="font-bold text-lg">{{headerText}}</h1>
+			<p class="text-sm">Or <span class="underline cursor-pointer" v-on:click="toggleView">{{subheadingText}} &#8594;</span></p>
+		</div>
+		<div class="space-y-5 max-w-lg">
+			<div class="mr-14">
+				<label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+				<div class="mt-1">
+					<input v-model="email" type="email" name="email" id="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com" />
+				</div>
+			</div>
+			<div class="mr-14">
+				<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+				<div class="mt-1">
+					<input v-model="password" type="password" name="password" id="password" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="••••" />
+				</div>
+			</div>
+			<button v-on:click="buttonClick" type="button" class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-storjBlue focus:outline-none focus:ring-2 focus:ring-offset-2">
+				{{buttonText}}
+			</button>
+		</div>
+	</div>
+</div>
 </template>
 
 <script lang="ts">
