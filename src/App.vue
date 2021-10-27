345c2b9e883cbbf79ea77c99f04de6a0bd0dab98
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-10-27 14:23:08 -0400

    Created `My Buckets` page

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-	<router-view />
+	<div class="bg-white m-auto overflow-hidden h-screen">
+		<img
+			class="absolute p-5 inset-0 h-auto w-1/3"
+			src="@/assets/logo.svg"
+		/>
+		<router-view />
+	</div>
 </template>
