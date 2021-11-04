b04bb50863cfd732eaf12fcd4d799b2ffae61856
Monty Anderson <work@montyanderson.net>
2021-11-04 19:20:16 +0000

    initial restic integration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,22 @@
 import { createStore } from "vuex";
 
-export default createStore({
-	state: {},
-	mutations: {},
+const store = createStore({
+	state: {
+		snapshots: []
+	},
+	mutations: {
+		setSnapshots(state, snapshots) {
+			state.snapshots = snapshots;
+		}
+	},
 	actions: {},
 	modules: {}
 });
+
+export default store;
+
+setTimeout(async () => {
+	// @ts-ignore
+	store.commit("setSnapshots", await window.backend.invoke("snapshots"));
+	console.log(store.state);
+}, 500);
