cef823ad1c99edbc32e1821b8f3e53a75a1e6703
Gabriel De Almeida <52146328+gabedealmeida@users.noreply.github.com>
2021-11-09 09:40:12 -0500

    Initial Settings page (#29)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-	<div class="bg-gray-50 m-auto overflow-hidden h-screen">
+	<div>
 		<img
+			v-if="logoPath"
 			class="absolute p-5 inset-0 h-auto w-1/3"
 			src="@/assets/logo.svg"
 		/>
-		<router-view />
+		<router-view></router-view>
 	</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "App",
+	computed: {
+		logoPath(): boolean {
+			const name: string = String(this.$route.name);
+			return name === "Home" || name === "Backups";
+		}
+	}
+});
+</script>
