e671990b0237ba3ff4e6ed94c6942a1616481f46
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-11-29 13:55:16 -0500

    Refactored Restore component to match mocks.

--- src/components/Restore.vue
+++ src/components/Restore.vue
@@ -8,15 +8,31 @@
 			<h1 class="text-lg font-bold">Restore</h1>
 		</div>
 
-		<div class="mt-5" v-if="location">
+		<div class="mt-5">
 			<p class="text-lg font-medium">Location</p>
-			<p class="text-xs font-normal text-gray-700 -mt-1">
-				Where your backup will be restored to
+			<p class="text-sm font-normal text-gray-700 mt-1">
+				Folder where your backup will be restored to
 			</p>
-			<p class="text-gray-700 text-sm mt-2">{{ location }}</p>
+			<div
+				class="
+					flex
+					justify-start
+					content-center
+					items-center
+					overflow-ellipsis
+					mt-1
+				"
+				v-if="location"
+			>
+				<img
+					class="w-5 h-5 fill-current text-black mr-2"
+					src="@/assets/folderIcon.svg"
+				/>
+				<p class="text-gray-700 font-normal text-sm">{{ location }}</p>
+			</div>
 		</div>
 
-		<div class="space-x-4">
+		<div class="flex items-end space-x-4 mt-5">
 			<button
 				type="button"
 				class="
@@ -24,20 +40,17 @@
 					items-center
 					px-3
 					py-2
-					mt-8
-					border border-transparent
+					h-full
+					border border-gray-300
+					shadow-sm
 					text-sm
 					leading-4
-					font-medium
+					font-semibold
 					rounded-md
-					shadow-sm
-					text-white
-					bg-storjBlue
-					hover:storjBlue
-					focus:outline-none
-					focus:ring-2
-					focus:ring-offset-2
-					focus:ring-storjBlue
+					text-gray-700
+					bg-white
+					hover:bg-gray-50
+					focus:outline-none focus:ring-2 focus:ring-offset-2
 				"
 				v-on:click="chooseLocation"
 			>
@@ -49,39 +62,15 @@
 				class="
 					inline-flex
 					items-center
-					px-3
+					px-4
 					py-2
-					mt-8
-					border border-transparent
+					h-full
+					border
+					shadow-sm
 					text-sm
 					leading-4
-					font-medium
+					font-semibold
 					rounded-md
-					shadow-sm
-					text-white
-					bg-blue-200
-					focus:outline-none
-					cursor-not-allowed
-				"
-				v-if="restoreDisabled"
-			>
-				Restore
-			</button>
-
-			<button
-				type="button"
-				class="
-					inline-flex
-					items-center
-					px-3
-					py-2
-					mt-8
-					border border-transparent
-					text-sm
-					leading-4
-					font-medium
-					rounded-md
-					shadow-sm
 					text-white
 					bg-storjBlue
 					hover:storjBlue
@@ -90,9 +79,10 @@
 					focus:ring-offset-2
 					focus:ring-storjBlue
 				"
-				v-else
+				v-if="!restoreDisabled"
 				v-on:click="restore"
 			>
+				<DownloadIcon class="w-4 h-4 mr-2 font-bold text-white" />
 				Restore
 			</button>
 		</div>
@@ -109,6 +99,7 @@ import { useRouter, useRoute } from "vue-router";
 import { useStore } from "@/store";
 
 import { ArrowLeftIcon } from "@heroicons/vue/solid";
+import { DownloadIcon } from "@heroicons/vue/outline";
 
 interface Properties {
 	locationInputElement: Ref<null | HTMLInputElement>;
@@ -126,7 +117,8 @@ interface Properties {
 export default defineComponent({
 	name: "Restore",
 	components: {
-		ArrowLeftIcon
+		ArrowLeftIcon,
+		DownloadIcon
 	},
 	setup: (): Properties => {
 		const store = useStore();
