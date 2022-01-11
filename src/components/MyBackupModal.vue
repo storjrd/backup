258607ae3e438046f67397e66a454ba1466e18b7
Gabriel De Almeida <hello@gabrieldealmeida.com>
2022-01-11 09:32:40 -0500

    Added check inside of the foldersArr computed property to throw an error if a folderName is actually undefined.

--- src/components/MyBackupModal.vue
+++ src/components/MyBackupModal.vue
@@ -372,7 +372,19 @@ export default defineComponent({
 		const selectedFrequency = ref("Daily");
 		const passphrase = ref("");
 
-		const foldersArr = computed(() => Object.keys(folders));
+		const foldersArr = computed(() => {
+			const foldersName = Object.keys(folders);
+
+			if (
+				foldersName.some(
+					(folderName) => typeof folderName === "undefined"
+				)
+			) {
+				throw new Error("Folder does not exist.");
+			}
+
+			return foldersName;
+		});
 		const foldersExist = computed(() => foldersArr.value.length > 0);
 		const selectFolderView = computed(() => view.value === "SELECT_FOLDER");
 		const frequencyView = computed(() => view.value === "FREQUENCY");
